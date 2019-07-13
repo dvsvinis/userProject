f548681b240d094decbff7ea9e2c6275fb377ec9
donnasvinis <dvsvinis@mac.com>
2019-07-12 22:36:23 -0400

    angular http get working

---
+++ user-ui/src/app/components/admin/admin.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+public users;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.userService.getUsers().subscribe(
+      data => { this.users = data},
+      err => console.error(err),
+      () => console.log('users loaded')
+    );
+  }
+
+}
