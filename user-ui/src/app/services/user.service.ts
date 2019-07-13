b9e279dae1ca3ec4cb9319a08365611c9cdcf6b2
donnasvinis <dvsvinis@mac.com>
2019-07-13 07:16:51 -0400

    User Registration form created

--- user-ui/src/app/services/user.service.ts
+++ user-ui/src/app/services/user.service.ts
@@ -11,17 +11,17 @@ const httpOptions = {
 })
 export class UserService {
 
-  constructor(private http:HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   getUsers() {
     return this.http.get('/server/api/v1/users');
   }
 
-  getUser(id: number){
+  getUser(id: number) {
     return this.http.get('/server/api/v1/users' + id);
   }
 
-  createUser(user){
+  createUser(user) {
     const body = JSON.stringify(user);
     return this.http.post('/server/api/v1/users' + body, httpOptions);
   }
